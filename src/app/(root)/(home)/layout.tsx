4487775e317ea1acd477d0740d28891efee4a888
Ravindra <sirviravindra609@gmail.com>
2024-05-18 12:21:12 +0530

    feat: Add Navbar component for home layout

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import React from "react";
 
@@ -5,7 +6,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative">
       {" "}
-      Navbar
+      <Navbar />
       <div className="flex">
         <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-white font-extrabold text-[26x] max-sm:hidden">
+          Operant Video Conferencing
+        </p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
