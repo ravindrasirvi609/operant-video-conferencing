00b93e7bf8f30322951b37c5094c3969d1fedb5d
Ravindra <sirviravindra609@gmail.com>
2024-05-18 23:22:53 +0530

    chore: Update home page layout and components, add MeetingTypeList component

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,11 +1,34 @@
+import MeetingTypeList from "@/components/MeetingTypeList";
 import React from "react";
 
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
+
   return (
     <section className="flex flex-col size-full gap-10 text-white">
-      <h1 className="text-3xl font-bold" id="home">
-        Home
-      </h1>
+      <div className="h-[300px] w-full bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:7xl">11:30 AM</h1>
+            <p className="text-lg font medium text-sky-1 lg:text-2xl">
+              Saturday, March 20, 2023
+            </p>
+          </div>
+        </div>
+      </div>
+      <MeetingTypeList />
     </section>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,53 +22,26 @@ const config = {
         dark: {
           1: "#1C1F2E",
           2: "#161925",
+          3: "#252A41",
+          4: "#1E2757",
         },
         blue: {
           1: "#0E78F9",
-          2: "#1E2A47",
         },
-        light: {
-          DEFAULT: "hsl(var(--light))",
-          foreground: "hsl(var(--light-foreground))",
+        sky: {
+          1: "#C9DDFF",
+          2: "#ECF0FF",
+          3: "#F5FCFF",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+        orange: {
+          1: "#FF742E",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
+        purple: {
+          1: "#830EF9",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        yellow: {
+          1: "#F9A90E",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
@@ -83,6 +56,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        hero: "url('/images/hero-background.png')",
       },
     },
   },
