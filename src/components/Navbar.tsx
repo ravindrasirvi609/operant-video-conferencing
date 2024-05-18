9cf947ef17af6f522a7caf7cf9c5ecdffad5fe25
Ravindra <sirviravindra609@gmail.com>
2024-05-18 13:28:38 +0530

    chore: Update dependencies and add MobileNav component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href={"/"} className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -17,6 +18,10 @@ const Navbar = () => {
           Operant Video Conferencing
         </p>
       </Link>
+
+      <div className="flex-between gap-5">
+        <MobileNav />
+      </div>
     </nav>
   );
 };
