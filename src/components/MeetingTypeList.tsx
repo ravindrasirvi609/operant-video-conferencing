a32c77056e843a4da02eac4bf22c70d9f91f4ad8
Ravindra <sirviravindra609@gmail.com>
2024-05-19 08:40:38 +0530

    feat: Add MeetingModal component for displaying meeting details and scheduling meetings

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -2,12 +2,16 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+
+  const createMeeting = async () => {};
+
   return (
     <section className="grid grid-col-1 gap-5 md:grid-cols-4 ">
       <HomeCard
@@ -36,6 +40,15 @@ const MeetingTypeList = () => {
         description="Meeting Recordings"
         className="bg-yellow-1"
         handleClick={() => router.push("/recordings")}
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className=" text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
