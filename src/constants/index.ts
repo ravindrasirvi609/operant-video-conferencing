c4462f244f98ee62d75334015457aebeb605e377
Ravindra <sirviravindra609@gmail.com>
2024-06-09 12:48:17 +0530

    feat: Add Loader component for displaying a loading spinner
    This commit adds a new Loader component to the project. The Loader component displays a loading spinner using the next/image package. It can be used to indicate that a page or component is loading. The Loader component is implemented in the Loader.tsx file.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,22 +1,36 @@
 export const sidebarLinks = [
   {
-    label: "Home",
-    route: "/",
     imgURL: "/icons/Home.svg",
+    route: "/",
+    label: "Home",
   },
+
   {
-    label: "Upcoming",
+    imgURL: "/icons/upcoming.svg",
     route: "/upcoming",
-    imgURL: "/icons/Upcoming.svg",
+    label: "Upcoming",
   },
   {
-    label: "Previous",
+    imgURL: "/icons/previous.svg",
     route: "/previous",
-    imgURL: "/icons/Previous.svg",
+    label: "Previous",
   },
   {
-    label: "Recordings",
+    imgURL: "/icons/Video.svg",
     route: "/recordings",
-    imgURL: "/icons/video.svg",
+    label: "Recordings",
   },
+  {
+    imgURL: "/icons/add-personal.svg",
+    route: "/personal-room",
+    label: "Personal Room",
+  },
+];
+
+export const avatarImages = [
+  "/images/avatar-1.jpeg",
+  "/images/avatar-2.jpeg",
+  "/images/avatar-3.png",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
 ];
